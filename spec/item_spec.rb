e9f0bb2775c830fe05c5eb93b31994b6027a14d6
Micha Bernhard <mib70@pitt.edu>
2021-09-22 10:26:14 -0600

    Test - Write tests for update methods

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -40,5 +40,25 @@ describe 'Item' do
     it 'has a merchant_id' do
       expect(@item.merchant_id).to eq(2)
     end
+
+    it '#update_name' do
+      @item.update_name('Pen')
+      expect(@item.name).to eq('Pen')
+    end
+
+    it '#update_description' do
+      @item.update_description('This is a description')
+      expect(@item.description).to eq('This is a description')
+    end
+
+    it '#update_unit_price' do
+      @item.update_unit_price(BigDecimal(999) / 100)
+      expect(@item.unit_price).to eq(0.999e1)
+    end
+
+    it '#update_updated_at' do
+      @item.update_updated_at
+      expect(@item.updated_at).to_not eq(@updated_at)
+    end
   end
 end
